9eca0a59de0b148da149313f8589e1b3c1ee7585
hendisantika <hendisantika@yahoo.co.id>
2025-01-14 06:22:52 +0700

    Add setup in BeforeEach🫸🌀✏️📗🐧🐳⬆

--- src/test/java/id/my/hendisantika/vertx_post_service/TestMainVerticle.java
+++ src/test/java/id/my/hendisantika/vertx_post_service/TestMainVerticle.java
@@ -1,18 +1,28 @@
 package id.my.hendisantika.vertx_post_service;
 
 import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpClientOptions;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import java.net.http.HttpClient;
+import java.util.logging.Logger;
+
 @ExtendWith(VertxExtension.class)
 public class TestMainVerticle {
+  private final static Logger LOGGER = Logger.getLogger(TestMainVerticle.class.getName());
+  HttpClient client;
+
 
   @BeforeEach
-  void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
-    vertx.deployVerticle(new MainVerticle()).onComplete(testContext.succeeding(id -> testContext.completeNow()));
+  void setup(Vertx vertx, VertxTestContext testContext) {
+    vertx.deployVerticle(new MainVerticle(), testContext.succeeding(id -> testContext.completeNow()));
+    var options = new HttpClientOptions()
+      .setDefaultPort(8888);
+    this.client = vertx.createHttpClient(options);
   }
 
   @Test
