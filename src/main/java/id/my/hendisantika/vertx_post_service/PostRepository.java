feff0205d2ff0337cccdf462cc77f330bacb0904
hendisantika <hendisantika@yahoo.co.id>
2025-01-14 06:15:17 +0700

    Add PostRepository🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/vertx_post_service/PostRepository.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.vertx_post_service;
+
+import java.util.logging.Logger;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : vertx-post-service
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 14/01/25
+ * Time: 06.15
+ * To change this template use File | Settings | File Templates.
+ */
+public class PostRepository {
+  private static final Logger LOGGER = Logger.getLogger(PostRepository.class.getName());
+}
